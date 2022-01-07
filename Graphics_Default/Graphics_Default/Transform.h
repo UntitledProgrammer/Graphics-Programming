07ecf581bab4790ce326dfce70e360cf0a5c5922
Tom Jacobs <thomasjacobs0257@gmail.com>
2022-01-07 15:28:58 +0000

    Transform component.
    Added, A simple transform component with a return method for representing it's three components in one matrix.

---
+++ Graphics_Default/Graphics_Default/Transform.h
@@ -0,0 +1,43 @@
+#ifndef TRANSFORM_H
+#define TRANSFORM_H
+
+#include<glm.hpp>
+#include<gtx/transform.hpp>
+
+struct Transform
+{
+public:
+	//Components:
+	glm::vec3 position;
+	glm::vec3 rotation;
+	glm::vec3 scale;
+
+	//Constructor:
+	/// <summary>Scale will default to [1,1,1]. Position and Rotation will default to [0,0,0].</summary>
+	Transform() : position(glm::vec3(0,0,0)) , rotation(glm::vec3(0,0,0)), scale(glm::vec3(1,1,1)) {};
+	/// <param name="p :">Position</param> <param name="r :">Rotation</param><param name="s :">Scale</param>
+	Transform(glm::vec3 p, glm::vec3 r, glm::vec3 s) : position(p), rotation(r), scale(s) {};
+	
+	//Deconstructor:
+	~Transform();
+
+	//Methods:
+
+	/// <summary>Returns a matrix using the context of each component.</summary>
+	inline glm::mat4 matrix() const
+	{
+		//Convert transform components to matricies:
+		glm::mat4 posMatrix = glm::translate(position);
+		glm::mat4 scaleMatrix = glm::scale(scale);
+
+		//Calculate a matrix for each component of the three vector rotation.
+		glm::mat4 rotXMatrix = glm::rotate(rotation.x, glm::vec3(1, 0, 0));
+		glm::mat4 rotYMatrix = glm::rotate(rotation.y, glm::vec3(0, 1, 0));
+		glm::mat4 rotZMatrix = glm::rotate(rotation.z, glm::vec3(0, 0, 1));
+
+		//Return a matrix using the result of each vectors matrix.
+		return posMatrix * (rotZMatrix * rotYMatrix * rotXMatrix) * scaleMatrix;
+	}
+};
+
+#endif //!TRANSFORM_H
