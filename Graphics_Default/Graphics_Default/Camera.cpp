a37cc486d579f261975a0361cec01039f27e2f93
Tom Jacobs <thomasjacobs0257@gmail.com>
2022-01-14 11:25:12 +0000

    Camera update.

--- Graphics_Default/Graphics_Default/Camera.cpp
+++ Graphics_Default/Graphics_Default/Camera.cpp
@@ -7,7 +7,7 @@ glm::mat4 Camera::matrix()
     return glm::lookAt(transform.position, glm::vec3(0), up);
 }
 
-Camera::Camera() : transform(Transform()), perspective(glm::mat4()) { Recalculate(); };
+Camera::Camera() : transform(Transform()), perspective(glm::mat4(1)) { Recalculate(); };
 
 Camera::~Camera()
 {
